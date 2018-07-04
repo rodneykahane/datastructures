c97c43484a948e39c4b7838c67d4ca9ac46621e1
rodney <rodney@rodneykahan.es>
2018-07-04 11:38:57 -0400

    missing class curly bracket

--- learning-datastructures/src/ds/doublylinkedlist/DoublyLinkedList.java
+++ learning-datastructures/src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -3,45 +3,45 @@ package ds.doublylinkedlist;
 public class DoublyLinkedList {
 	private Node first;
 	private Node last;
-	
+
 	public DoublyLinkedList() {
 		this.first = null;
 		this.last = null;
 	}
-	
-		
+
+
 	public boolean isEmpty() {
 		return (first==null);
 	}
-	
+
 	public void insertFirst(int data) {
 		Node newNode = new Node();
 		newNode.data = data;
-		
+
 		if(isEmpty()) {
 			last = newNode;  //if empty, last will refer to the new node being created
 		} else {
 			first.previous = newNode;
 		}
-		
+
 		newNode.next = first; //the new node's next field will point to the old first
 		this.first = newNode;
 	}
-	
+
 	public void insertLast(int data) {
 		Node newNode = new Node();
 		newNode.data = data;
-		
+
 		if(isEmpty()) {
 			first = newNode;
 		} else {
 			last.next = newNode; //assigning old last to newNode
 			newNode.previous = last; //old last will be newNode's previous
 		}
-			last = newNode; //the linkedlist's last field should point to the new node
+		last = newNode; //the linkedlist's last field should point to the new node
 	}
-	
-	 //assume non-empty list
+
+	//assume non-empty list
 	public Node deleteFirst() {
 		Node temp = first;
 		if(first.next == null) { //there is only one item in the list
@@ -49,11 +49,11 @@ public class DoublyLinkedList {
 		} else {
 			first.next.previous = null;
 		}
-		
+
 		first = first.next; //we are assigning the reference of the node following the old first node to the first field in the linked list object
 		return temp;//returned the deleted old first node		
 	}
-	
+
 	//assume non-empty list
 	public Node deleteLast() {
 		Node temp = last;
@@ -63,12 +63,12 @@ public class DoublyLinkedList {
 		} else {
 			last.previous.next = null;
 		}
-		
+
 		last = last.previous;		
 		return temp;
 	}
-	
-	
+
+
 	//lecture 16
 	public boolean insertAfter(int key, int data) {
 		Node current = first;
@@ -78,10 +78,10 @@ public class DoublyLinkedList {
 				return false;
 			}//end if
 		}//end while
-		
+
 		Node newNode = new Node();
 		newNode.data = data;
-		
+
 		if(current == last) {
 			current.next = null;
 			last = newNode;
@@ -89,36 +89,35 @@ public class DoublyLinkedList {
 			newNode.next = current.next;
 			current.next.previous = newNode;
 		}
-		
+
 		newNode.previous = current;
 		current.next = newNode;
-		
+
 		return true;
-		
+
 	}
-	
-//assume non-empty list
+
+	//assume non-empty list
 	public Node deleteKey(int key) {
 		Node current = new Node();
-		
+
 		current = first;
 		while(current.data != key) {
 			current = current.next;
 			if(current==null) {
 				last.previous.next=null;
 			}
-		}
-		
-				
+		}		
+
 		if(current==last) {
 			first = null;
 		} else {
-		
-		
-		
-		
-	}
- 
-	
 
+
+
+
+		} 
+
+
+	}
 }
